6c2bcf29187cef194ec6ec4dc2b05aac47f9c830
Nicole Sattler <nicole.sattler@live.com>
2017-06-23 20:11:29 +0000

    Added a 'back' button to delivery status page

--- send.php
+++ send.php
@@ -8,9 +8,10 @@ $message = $_POST['message'];
 $retval = mail ($to, $subject, $message, 'From:'.$from);
 
 if($retval == true) {
-    echo "Message sent successfully!";
+    echo 'Message sent successfully! <br /> <a href="index.html">Back</a>';
+
 } else {
-    echo "Uh-oh! Message could not be sent.";
+    echo 'Uh-oh! Message could not be sent. <br /> <a href="index.html">Back</a>';
 }
 
 ?>
